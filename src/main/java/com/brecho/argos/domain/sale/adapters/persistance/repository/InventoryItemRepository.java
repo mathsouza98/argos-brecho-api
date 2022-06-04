090dedefa1205b5e358d63dbb108594074a2dcb5
Matheus Souza <matheus.98.2013@gmail.com>
2022-06-04 20:18:01 -0300

    feat: enhance repositories

--- src/main/java/com/brecho/argos/domain/sale/adapters/persistance/repository/InventoryItemRepository.java
+++ src/main/java/com/brecho/argos/domain/sale/adapters/persistance/repository/InventoryItemRepository.java
@@ -2,8 +2,15 @@ package com.brecho.argos.domain.sale.adapters.persistance.repository;
 
 import com.brecho.argos.domain.sale.adapters.persistance.entity.InventoryItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InventoryItemRepository extends JpaRepository<InventoryItemEntity, String> {
+
+    @Query("select * from InventoryItemEntity where product_id in :ids and status = 1")
+    List<InventoryItemEntity> findAvailableInventoryItemsByProductsIds(@Param("ids") List<String> ids);
 }
--- src/main/java/com/brecho/argos/domain/sale/adapters/persistance/repository/SaleItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brecho.argos.domain.sale.adapters.persistance.repository;
-
-import com.brecho.argos.domain.sale.adapters.persistance.entity.SaleItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleItemRepository extends JpaRepository<SaleItemEntity, String> {
-}
